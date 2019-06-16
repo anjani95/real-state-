c2397909a2ade58d2b6578b8c704b71b79bd92ff
real-state <anjani.bloombytes@gmail.com>
2019-06-16 20:32:31 +0530

    2nd commit

--- index.php
+++ index.php
@@ -6,5 +6,17 @@
     <h4>hello friend</h4>
    hello friend
 thank you
+
+<div class="container">
+   <div class="row">
+      <div class="col-ma-7">
+       <h4>hello friend</h4>
+        
+      </div>
+   
+   </div>
+
+
+</div>
 </body>
 </html>
